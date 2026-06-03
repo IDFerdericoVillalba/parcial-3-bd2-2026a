050bffd34dc75b5d3970fa1ef4728bb1fa1beb34
isaac <duranvillalba2005@gmail.com>
2026-06-02 23:40:22 -0500

    se modifico en la carpeta ddl, se modifico el archivo crear_bd.sql para actualizar la base de datos consultorio_medico

--- entregas/isaac-duran-proyecto04/ddl/crear_bd.sql
+++ entregas/isaac-duran-proyecto04/ddl/crear_bd.sql
@@ -8,19 +8,37 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
--- Schema citas_medicas2
+-- Schema consultorio_medico
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema citas_medicas2
+-- Schema consultorio_medico
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `citas_medicas2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
-USE `citas_medicas2` ;
+CREATE SCHEMA IF NOT EXISTS `consultorio_medico` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
+USE `consultorio_medico` ;
 
 -- -----------------------------------------------------
--- Table `citas_medicas2`.`paciente`
+-- Table `consultorio_medico`.`medicos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `citas_medicas2`.`paciente` (
+CREATE TABLE IF NOT EXISTS `consultorio_medico`.`medicos` (
+  `id_medico` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(80) NOT NULL,
+  `apellido` VARCHAR(80) NOT NULL,
+  `documento` VARCHAR(20) NOT NULL,
+  `telefono` VARCHAR(20) NOT NULL,
+  `correo` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id_medico`),
+  UNIQUE INDEX `uq_medico_registro` (`documento` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 27
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `consultorio_medico`.`pacientes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `consultorio_medico`.`pacientes` (
   `id_paciente` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(80) NOT NULL,
   `apellido` VARCHAR(80) NOT NULL,
@@ -32,31 +50,15 @@ CREATE TABLE IF NOT EXISTS `citas_medicas2`.`paciente` (
   PRIMARY KEY (`id_paciente`),
   UNIQUE INDEX `uq_pac_documento` (`documento` ASC) VISIBLE)
 ENGINE = InnoDB
+AUTO_INCREMENT = 25
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `citas_medicas2`.`medico`
+-- Table `consultorio_medico`.`citas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `citas_medicas2`.`medico` (
-  `id_medico` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(80) NOT NULL,
-  `apellido` VARCHAR(80) NOT NULL,
-  `documento` VARCHAR(20) NOT NULL,
-  `telefono` VARCHAR(20) NOT NULL,
-  `correo` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id_medico`),
-  UNIQUE INDEX `uq_medico_registro` (`documento` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
-
--- -----------------------------------------------------
--- Table `citas_medicas2`.`cita`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `citas_medicas2`.`cita` (
+CREATE TABLE IF NOT EXISTS `consultorio_medico`.`citas` (
   `id_cita` INT NOT NULL AUTO_INCREMENT,
   `id_paciente` INT NOT NULL,
   `id_medico` INT NOT NULL,
@@ -64,95 +66,91 @@ CREATE TABLE IF NOT EXISTS `citas_medicas2`.`cita` (
   `hora` TIME NOT NULL,
   `estado` ENUM('programada', 'atendida', 'cancelada', 'no_asistio') NOT NULL DEFAULT 'programada',
   `motivo` TEXT NOT NULL,
-  PRIMARY KEY (`id_cita`, `id_paciente`, `id_medico`),
+  PRIMARY KEY (`id_cita`),
   INDEX `fk_cita_paciente1_idx` (`id_paciente` ASC) VISIBLE,
   INDEX `fk_cita_medico1_idx` (`id_medico` ASC) VISIBLE,
-  CONSTRAINT `fk_cita_paciente1`
-    FOREIGN KEY (`id_paciente`)
-    REFERENCES `citas_medicas2`.`paciente` (`id_paciente`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_cita_medico1`
     FOREIGN KEY (`id_medico`)
-    REFERENCES `citas_medicas2`.`medico` (`id_medico`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `consultorio_medico`.`medicos` (`id_medico`),
+  CONSTRAINT `fk_cita_paciente1`
+    FOREIGN KEY (`id_paciente`)
+    REFERENCES `consultorio_medico`.`pacientes` (`id_paciente`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 40
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `citas_medicas2`.`consulta`
+-- Table `consultorio_medico`.`consultas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `citas_medicas2`.`consulta` (
+CREATE TABLE IF NOT EXISTS `consultorio_medico`.`consultas` (
   `id_consulta` INT NOT NULL AUTO_INCREMENT,
   `id_cita` INT NOT NULL,
   `diagnostico` TEXT NOT NULL,
   `observaciones` TEXT NULL DEFAULT NULL,
   `fecha_registro` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_consulta`, `id_cita`),
+  PRIMARY KEY (`id_consulta`),
   INDEX `fk_consulta_cita1_idx` (`id_cita` ASC) VISIBLE,
   CONSTRAINT `fk_consulta_cita1`
     FOREIGN KEY (`id_cita`)
-    REFERENCES `citas_medicas2`.`cita` (`id_cita`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `consultorio_medico`.`citas` (`id_cita`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 22
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `citas_medicas2`.`especialidad`
+-- Table `consultorio_medico`.`especialidades`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `citas_medicas2`.`especialidad` (
+CREATE TABLE IF NOT EXISTS `consultorio_medico`.`especialidades` (
   `id_especialidad` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(80) NOT NULL,
   `descripcion` TEXT NULL DEFAULT NULL,
   PRIMARY KEY (`id_especialidad`),
   UNIQUE INDEX `uq_esp_nombre` (`nombre` ASC) VISIBLE)
 ENGINE = InnoDB
+AUTO_INCREMENT = 5
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `citas_medicas2`.`horarios`
+-- Table `consultorio_medico`.`horarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `citas_medicas2`.`horarios` (
+CREATE TABLE IF NOT EXISTS `consultorio_medico`.`horarios` (
   `id_horario` INT NOT NULL AUTO_INCREMENT,
   `id_medico` INT NOT NULL,
   `dia_semana` TINYINT NOT NULL COMMENT '1=Lunes, 7=Domingo',
   `hora_inicio` TIME NOT NULL,
   `hora_fin` TIME NOT NULL,
-  PRIMARY KEY (`id_horario`, `id_medico`),
+  PRIMARY KEY (`id_horario`),
   INDEX `fk_horarios_medico1_idx` (`id_medico` ASC) VISIBLE,
   CONSTRAINT `fk_horarios_medico1`
     FOREIGN KEY (`id_medico`)
-    REFERENCES `citas_medicas2`.`medico` (`id_medico`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `consultorio_medico`.`medicos` (`id_medico`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 30
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `citas_medicas2`.`medico_especialidad`
+-- Table `consultorio_medico`.`medico_especialidad`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `citas_medicas2`.`medico_especialidad` (
+CREATE TABLE IF NOT EXISTS `consultorio_medico`.`medico_especialidad` (
   `id_medico` INT NOT NULL,
   `id_especialidad` INT NOT NULL,
   PRIMARY KEY (`id_medico`, `id_especialidad`),
   INDEX `fk_mesp_esp_idx` (`id_especialidad` ASC) VISIBLE,
   CONSTRAINT `fk_mesp_esp`
     FOREIGN KEY (`id_especialidad`)
-    REFERENCES `citas_medicas2`.`especialidad` (`id_especialidad`)
+    REFERENCES `consultorio_medico`.`especialidades` (`id_especialidad`)
     ON DELETE CASCADE,
   CONSTRAINT `fk_mesp_medico`
     FOREIGN KEY (`id_medico`)
-    REFERENCES `citas_medicas2`.`medico` (`id_medico`)
+    REFERENCES `consultorio_medico`.`medicos` (`id_medico`)
     ON DELETE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
@@ -160,20 +158,19 @@ COLLATE = utf8mb4_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `citas_medicas2`.`tratamiento`
+-- Table `consultorio_medico`.`tratamientos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `citas_medicas2`.`tratamiento` (
+CREATE TABLE IF NOT EXISTS `consultorio_medico`.`tratamientos` (
   `id_tratamiento` INT NOT NULL AUTO_INCREMENT,
   `id_consulta` INT NOT NULL,
   `medicamento` VARCHAR(150) NOT NULL,
-  PRIMARY KEY (`id_tratamiento`, `id_consulta`),
+  PRIMARY KEY (`id_tratamiento`),
   INDEX `fk_tratamiento_consulta1_idx` (`id_consulta` ASC) VISIBLE,
   CONSTRAINT `fk_tratamiento_consulta1`
     FOREIGN KEY (`id_consulta`)
-    REFERENCES `citas_medicas2`.`consulta` (`id_consulta`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `consultorio_medico`.`consultas` (`id_consulta`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 22
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
